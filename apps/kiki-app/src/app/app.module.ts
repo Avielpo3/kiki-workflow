27f49c463a62f4c57adec29de68fa9a63a66fec7
aviel <avielpo3@gmail.com>
2021-12-18 08:57:53 +0200

    Added Footer and header

--- apps/kiki-app/src/app/app.module.ts
+++ apps/kiki-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { UiModule } from "@kiki/ui";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, UiModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ libs/ui/src/lib/header/header.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "kiki-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() title: string = 'aaa';
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
