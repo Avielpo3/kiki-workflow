e17173cebcb0729ca99476008418f7fd504cab94
aviel <avielpo3@gmail.com>
2021-12-24 08:47:52 +0200

    pdfkit init

---
+++ libs/pdf-editor/README.md
@@ -0,0 +1,7 @@
+# pdf-editor
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test pdf-editor` to execute the unit tests.
---
+++ libs/pdf-editor/src/lib/pdf-editor/pdf-editor.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import PSPDFKit from 'pspdfkit';
+
+@Component({
+  selector: 'kiki-pdf-editor',
+  templateUrl: './pdf-editor.component.html',
+  styleUrls: ['./pdf-editor.component.scss'],
+})
+export class PdfEditorComponent implements OnInit {
+  //constructor() {}
+
+  ngOnInit(): void {
+    PSPDFKit.load({
+      // Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
+      baseUrl: location.protocol + '//' + location.host + '/assets/',
+      document: '/assets/example.pdf',
+      container: '#pspdfkit-container',
+    }).then((instance) => {
+      // For the sake of this demo, store the PSPDFKit for Web instance
+      // on the global object so that you can open the dev tools and
+      // play with the PSPDFKit API.
+      (window as any).instance = instance;
+    });
+  }
+}
